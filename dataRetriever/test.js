5260e360c385c2d48f57185bad45d0de2173cd7c
Thibault Brocherieux <tbrocherieux@ippon.fr>
2019-01-19 15:53:32 +0100

    1st working

--- dataRetriever/test.js
+++ dataRetriever/test.js
@@ -6,6 +6,8 @@ var directory = 'img/'
 global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 global.XMLHttpRequest.DONE = 4;
 
+var picManager = new P4C.PicturesManager();
+
 /*
 uri : download uri
 filename : output filename 
@@ -20,23 +22,32 @@ var download = function (uri, filename, callback) {
 /*
 filename : file to load (JSON)
 */
-var loadFile = function (filename){	
+var loadFile = function (filename) {
 	var contents = fs.readFileSync(filename);
 	// Define to JSON type
 	var jsonContent = JSON.parse(contents);
 }
 
-var picManager = new P4C.PicturesManager();
 
-picManager.startPicsRetrievalAround(new P4C.LatLng(48.0159348, 0.2034504), 15, { mindate: 0, towardscenter: true })
-	.then(function (pictures) {
-		for (var i = 0; i < pictures.length; i++) {
-			var pic = pictures[i];
-			download(pic.pictureUrl, directory + i + '.jpg', function () {
-				console.log('Downloaded');
-			});
-			console.log(pic.coordinates);
-			console.log(pic.direction);
+var main = function () {
+	var input = loadFile("input.json");
+
+	for (var i = 0; i < input.intersections.length; i++) {
+		for (var j = 0; j < input.intersections[i].direction.length; j++) {
+			picManager.startPicsRetrievalAround(new P4C.LatLng(input.intersections[i].direction[j].lat, input.intersections[i].direction[j].long), 15, { mindate: 0, towardscenter: true })
+				.then(function (pictures) {
+					for (var i = 0; i < pictures.length; i++) {
+						var pic = pictures[i];
+						download(pic.pictureUrl, directory + i + '.jpg', function () {
+							console.log('Downloaded');
+						});
+						console.log(pic.coordinates);
+						console.log(pic.direction);
+					}
+				});
 		}
+	}
 
-	});
+
+
+
